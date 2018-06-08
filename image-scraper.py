3c8b8e68cfa55248912aaf23543c5b5cea267bf7
Ryan <ryankurz123@gmail.com>
2018-06-08 19:49:48 +1000

    words and scrapes images

--- image-scraper.py
+++ image-scraper.py
@@ -4,28 +4,48 @@ from bs4 import BeautifulSoup
 import re
 import pandas
 import os
+import urllib
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
 
+
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = text.upper()
-    url = "http://google.com.au/search?q=" + text
-    run_scrape(url)
+    get_to_images(text)
     return processed_text
 
-def run_scrape(url):
+
+def get_to_images(keyword):
+    url = "http://google.com.au/search?q=" + keyword
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
     driver.get(url)
     images_button = driver.find_element_by_xpath("//*[text()='Images']")
     images_button.click()
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    alt = "Image result for " + keyword
+
+    count = 0
+
+    if not os.path.exists(keyword):
+        os.makedirs(keyword)
+
+    for img in soup.find_all('img', alt=alt):
+        imgUrl = img.get('data-src')
+        if imgUrl is not None:
+            imgName = keyword + "/" + keyword + "_" + str(count)
+            count += 1
+            urllib.urlretrieve(imgUrl, imgName)
+
 
 if __name__ == "__main__":
     app.run() 
