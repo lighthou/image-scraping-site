3cc5e06625acd08abcb629ffc407c2539c085581
Kurz <ryan.kurz@sap.com>
2018-06-13 12:54:57 +1000

    testing with constructed image

--- image-scraper.py
+++ image-scraper.py
@@ -22,7 +22,25 @@ def my_form_post():
     text = request.form['text']
     uploaded_file = request.files['file']
     uploaded_file.save(os.path.join("UPLOADS", uploaded_file.filename))
-    break_image_to_rgb("UPLOADS/" + uploaded_file.filename)
+    dic = break_image_to_rgb("UPLOADS/" + uploaded_file.filename)
+
+
+    
+    width = 50
+
+    
+    im = Image.new("RGB", (1024, 1024))
+    pix = im.load()
+    for (key, value) in dic.items():
+        for x in range(1024 // width * value[0], (1024 // width) * (value[0] + 1)):
+            for y in range (1024 // width * value[1], (1024 // width) * (value[1] + 1)):
+                #print(x,y, key)
+                pix[x,y] = key
+                
+    im.save("test.png", "PNG")
+
+
+    
     processed_text = text.upper()
     #scrape_images(text)
     return processed_text
@@ -33,7 +51,7 @@ def break_image_to_rgb(image):
     photo = photo.convert('RGB')
     photo_dictionary = {}
     width, height = photo.size
-    x_y_image_count = 10
+    x_y_image_count = 50
     divider = (width/x_y_image_count) * (height/x_y_image_count)
     for y in range (0, x_y_image_count):
         for x in range(0, x_y_image_count):
@@ -43,8 +61,8 @@ def break_image_to_rgb(image):
                     if x1 < width and y1 < height:
                         r, g, b = photo.getpixel((x1, y1))
                         tr, tg, tb = tr + r, tg + g, tb + b
-            photo_dictionary[x, y] = min(255, int(tr/divider)), min(int(tg/divider), 255), min(255, int(tb/divider))
-
+            photo_dictionary[min(255, int(tr/divider)), min(int(tg/divider), 255), min(255, int(tb/divider))] = x, y
+    return photo_dictionary
             
 
 def scrape_images(keyword):
